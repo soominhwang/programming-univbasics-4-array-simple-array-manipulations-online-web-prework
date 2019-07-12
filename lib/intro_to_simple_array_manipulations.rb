c17c846ca72ec4b44d10ba1e48a48627497fb5bf
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-12 06:18:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,18 @@ def using_concat(my_favorite_things,more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert
-
+def using_insert(list_of_programming_languages,another_language)
+  list_of_programming_languages.insert(another_language)
 end
+
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
+  end
+
+  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do
+    expect(@new_array[4]).to eq(@another_language)
+  end
+
+  it "increases the length of the array" do
+    expect(@new_array.length).to eq(8)
