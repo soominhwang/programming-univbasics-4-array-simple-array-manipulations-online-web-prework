ede0ea454a581414081a68c48b2d50d17f2d4ec8
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-12 06:16:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,15 +25,3 @@ end
 def using_concat(my_favorite_things,more_favs)
   my_favorite_things.concat(more_favs)
 end
-    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports cars", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
-  end
-   it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do
-    expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"])
-   end
-
-   it "increases the length of the first array" do
-    expect(@my_favorite_things.length).to be > 2
-   end
-end
