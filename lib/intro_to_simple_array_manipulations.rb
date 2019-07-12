6a97c6bb97a38a8f4f255a6bd1f8820d25166c10
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-12 06:29:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,7 @@ def using_flatten(instruments)
   instruments.flatten
 end
 
-def using_delete (instructors,"Steven")
+def using_delete(instructors, "Steven")
   instructors.delete("Steven")
+
 end
