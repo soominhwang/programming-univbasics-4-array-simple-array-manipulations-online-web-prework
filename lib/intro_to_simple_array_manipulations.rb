b06b5513779d70bc1ed9eb671d6329442d3051dd
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-12 06:21:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,12 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4,another_language)
 end
 
-def
-  
+def using_uniq(haircuts)
+
+end
+  it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
+    haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+    new_array = using_uniq(haircuts)
+    expect(new_array).to eq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"])
+  end
 end
